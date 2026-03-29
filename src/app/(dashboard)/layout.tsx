ecd571c412fbe4170217dfc2491503c458544ab6
Muhammad Tayab Noor <60859881+mtayabnoor@users.noreply.github.com>
2026-03-30 01:47:53 +0200

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset>
+      <SidebarInset className="flex flex-col h-full overflow-hidden">
         <SiteHeader />
         <main className="flex-1 overflow-y-auto w-full max-w-7xl mx-auto px-5 md:px-10">
           {children}
