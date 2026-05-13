0e20cda790a55d2a0b84b28cdd5060647c0e791b
muno1623 <mtayabnoor@gmail.com>
2026-05-13 17:22:25 +0200

    fix: Fix user name icon in app sidebar foot menu item

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -146,9 +146,9 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 <DropdownMenuGroup>
                   <DropdownMenuLabel className="p-0 font-normal">
                     <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                      <Avatar className="h-8 w-8 rounded-lg">
-                        <AvatarFallback className="rounded-lg">{user?.name?.charAt(0)}</AvatarFallback>
-                      </Avatar>
+                      <div className="flex h-8 w-8 shrink-0 items-center justify-center rounded-full bg-primary text-md font-semibold text-black">
+                        {user?.name?.charAt(0)?.toUpperCase()}
+                      </div>
                       <div className="grid flex-1 text-left text-sm leading-tight">
                         <span className="truncate font-medium">{user?.name}</span>
                         <span className="truncate text-xs text-muted-foreground">{user?.email}</span>
